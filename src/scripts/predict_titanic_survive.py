e32a16adcca529e809eb89cd2fcb618a9f27a64f
312076 <ajay.sant@wu.com>
2019-04-08 18:04:20 +0530

    moved the csv files from resources to input directory.

--- src/scripts/predict_titanic_survive.py
+++ src/scripts/predict_titanic_survive.py
@@ -17,12 +17,17 @@ from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
 
 addpoly = True
-plot_lc = 0   # 1--display learning curve/ 0 -- don't display
+plot_lc = 0  # 1--display learning curve/ 0 -- don't display
 
-#loading the data sets from the csv files
+# loading the data sets from the csv files
 print('--------load train & test file------')
-train_dataset = pd.read_csv('../input/train.csv')
-test_dataset = pd.read_csv('../input/test.csv')
+train_dataset = pd.read_csv(
+    '/Users/312076/Desktop/Learning/git_hub/titanic-machine-learning-from-disaster/input/train.csv')
+test_dataset = pd.read_csv(
+    '/Users/312076/Desktop/Learning/git_hub/titanic-machine-learning-from-disaster/input/test.csv')
 
-print('train dataset: %s, test dataset %s' %(str(train_dataset.shape), str(test_dataset.shape)) )
+print('train dataset: %s, test dataset %s' % (str(train_dataset.shape), str(test_dataset.shape)))
 train_dataset.head()
+
+train_dataset.head()
+
