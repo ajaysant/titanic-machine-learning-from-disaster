8522db54e6fb2c8d2b5cd6e4727000f00899be93
312076 <ajay.sant@wu.com>
2019-04-08 18:09:35 +0530

    moved the csv files from resources to input directory.

--- src/scripts/predict_titanic_survive.py
+++ src/scripts/predict_titanic_survive.py
@@ -22,12 +22,14 @@ plot_lc = 0  # 1--display learning curve/ 0 -- don't display
 # loading the data sets from the csv files
 print('--------load train & test file------')
 train_dataset = pd.read_csv(
-    '/Users/312076/Desktop/Learning/git_hub/titanic-machine-learning-from-disaster/input/train.csv')
+    '/input/train.csv')
 test_dataset = pd.read_csv(
-    '/Users/312076/Desktop/Learning/git_hub/titanic-machine-learning-from-disaster/input/test.csv')
+    '/input/test.csv')
 
 print('train dataset: %s, test dataset %s' % (str(train_dataset.shape), str(test_dataset.shape)))
 train_dataset.head()
 
-train_dataset.head()
+print(train_dataset.head())
+
+print('Id is unique.') if train_dataset.PassengerId.nunique() == train_dataset.shape[0] else print('oops')
 
